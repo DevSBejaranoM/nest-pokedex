0b7864bbc28b8df17149e0a643bb06fc61d3acd4
Sergio Bejarano Martín <dev.sbejaranom@gmail.com>
2025-02-20 22:22:46 +0100

    app

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,6 @@
 export const EnvConfigutation = () => ({
   enviroment: process.env.NODE_ENV || 'development',
-  mongodb: process.env.MONGODB || '',
+  mongodb: process.env.MONGODB || 'mongodb://mongo:yshtXplcWmXeKrFVRkQRfBVoUdwXpWyk@gondola.proxy.rlwy.net:55681',
   port: process.env.PORT || 3000,
   defaultLimit: process.env.DEFAULT_LIMIT || 10,
 });
